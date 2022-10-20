3b41e022eee116d8c06faa3466d89debb712db20
mraoofnia <m.raoofnia@bluteam.ir>
2022-10-20 21:33:18 +0330

    Initiating project

---
+++ src/test/java/ir/mraoofnia/daftar/DaftarApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mraoofnia.daftar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaftarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
